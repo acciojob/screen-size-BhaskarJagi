2de15f70877909d51154203a893a843700e03978
BhaskarJagi <128607429+BhaskarJagi@users.noreply.github.com>
2023-08-04 22:41:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const size = document.getElementsByTagName('h1')[0]
+
+
+window.addEventListener('resize',() => {
+	document.getElementById('width').innerText= window.innerWidth
+	document.getElementById('height').innerText= window.innerHeight
+})
+
+
